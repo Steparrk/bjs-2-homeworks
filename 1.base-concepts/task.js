b6b3a5d7824a3c1bdbfbd8e77ef576c583853dbd
Arkadiy Stepanov <Steparrks@gmail.com>
2022-07-05 23:13:04 +0700

    Complete 1 HW

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,45 @@
+'use strict';
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+
+	let arr = [];
+
+	let d = Math.pow(b, 2) - 4 * a * c;
+
+	if(d == 0) {
+		arr.push(-b / 2 * a);
+	}
+	if(d > 0 && a != 0) {
+		arr.push((-b + Math.sqrt(d)) / (2 * a));
+		arr.push((-b - Math.sqrt(d)) / (2 * a));
+	}
+
+	return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
+	let totalAmount;
+	let lastMonth = date.getMonth();
+	let lastYear = date.getFullYear();
+	let currentDate = new Date();
+	let currentMonth = currentDate.getMonth();
+	let currentYear = currentDate.getFullYear();
 
-  // код для задачи №2 писать здесь
+	if (isNaN(percent)) {
+		return (`Параметр "Процентная ставка" содержит неправильное значение "${percent}"`);
+	}
+	if (isNaN(contribution)) {
+		return (`Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`);
+	}
+	if (isNaN(amount)) {
+		return (`Параметр "Общая стоимость" содержит неправильное значение "${amount}"`);
+	}
 
-  return totalAmount;
+	let S = amount - contribution;
+	let n = ((lastYear - currentYear) * 12) + (lastMonth - currentMonth);
+	let P = (percent / 100) / 12;
+	let amountMonth = S * (P + (P / (((1 + P) ** n) - 1)));
+	totalAmount = Number((amountMonth * n).toFixed(2));
+
+	return totalAmount;
 }
