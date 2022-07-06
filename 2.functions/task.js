acbc7bdd7fe5e6e254d651a8751163f419063494
Arkadiy Stepanov <Steparrks@gmail.com>
2022-07-06 21:33:19 +0700

    Complete function HW

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,62 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min = arr[0]; 
+  let max = arr[0];
+  let sum = 0; 
+  let avg = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if(arr[i] > max) {
+      max = arr[i];
+    }
+    if(arr[i] < min) {
+      min = arr[i];
+    }
+    sum = sum + arr[i];
+  }
 
-  // Ваш код
-
+  avg = Number((sum / arr.length).toFixed(2));
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
-  // Ваш код
+  for(const i in arr) {
 
+    sum += arr[i];
+  }
+  console.log(sum)
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = func(arrOfArr[0]);
 
-  // Ваш кода
-  // for ...
-  
+  for(const i in arrOfArr) {
+
+    if (func(arrOfArr[i]) > max) {
+      max = func(arrOfArr[i]);
+    }
+  }
+
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min = arr[0]; 
+  let max = arr[0];
+  let difference = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if(arr[i] > max) {
+      max = arr[i];
+    }
+    if(arr[i] < min) {
+      min = arr[i];
+    }
+  }
+  difference = Math.abs(min - max);
+
+  return difference;
 }
