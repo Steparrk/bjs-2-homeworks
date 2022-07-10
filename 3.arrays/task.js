32fd3cb15cd8143e8870dc9636013b6cc753e29e
Arkadiy Stepanov <Steparrks@gmail.com>
2022-07-10 16:28:58 +0700

    Complete arrays HW

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,13 @@
 function compareArrays(arr1, arr2) {
-  let result;
 
-  // Ваш код
+	let result = arr1.length === arr2.length && arr1.every((a, i) => a === arr2[i]);
 
-  return result; // boolean
+	return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
 
-  // Ваш код
+	let resultArr = arr.filter((number) => number > 0).filter((number) => number % 3 ===0).map((number) => number * 10);
 
-  return resultArr; // array
+	return resultArr; // array
 }
