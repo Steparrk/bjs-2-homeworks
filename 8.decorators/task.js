d0dacb83ea81663457405c1cfc307522974bb9dc
Arkadiy Stepanov <Steparrks@gmail.com>
2022-08-07 19:07:53 +0700

    Initial commit HW decorators

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,60 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+	const cache = [];
+	return function(...args) {
+		const hash = args.join(",");
+		let cacheElement = cache.find((item) => item.hash === hash);
+		if(cache.length > 5) {
+			cache.splice(0, 1);
+		}
+		if(cacheElement === undefined) {
+				let result = func.call(this, ...args);
+				cache.push({hash, result});
+				console.log(`Вычисляем: ${result}`)
+				return `Вычисляем: ${result}`;
+		}else{
+			console.log(`Из кэша: ${cacheElement.result}`)
+			return `Из кэша: ${cacheElement.result}`;
+		}
+	}
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, ms) {
+	let LastCallTimestamp = null;
+
+	return function(...args) {
+		const now = new Date();
+		if(LastCallTimestamp === null) {
+			func.call(this, ...args);
+			LastCallTimestamp = now;
+			return;
+		}
+		if(LastCallTimestamp !== null && now - LastCallTimestamp > ms){
+			func.call(this, ...args);
+			console.log(now - LastCallTimestamp, ms);
+			LastCallTimestamp = now;
+			return;
+		}
+	}
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+function debounceDecorator2(func, ms) {
+	let LastCallTimestamp = null;
+	let count = 0;
+	wrapper.history = [];
+	function wrapper(...args) {
+		const now = new Date();
+		if(LastCallTimestamp === null) {
+			count++;
+			LastCallTimestamp = now;
+			return func.call(this, ...args);
+		}
+		if(LastCallTimestamp !== null && now - LastCallTimestamp > ms){
+			count++;
+			LastCallTimestamp = now;
+			wrapper.history.splice(0, 1, count);
+			return func.call(this, ...args);
+		}
+	}
+	return wrapper;
 }
